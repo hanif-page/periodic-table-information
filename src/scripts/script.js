19eef1960b5add83d635e0e30920d349c7c0ee6d
hanif-page <amrhanif15@gmail.com>
2022-03-31 16:07:40 +0700

    update the API link (the old one was not getting developed anymore), add new resetElementInformation function, and add some more styles on the card overlay

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,8 +1,8 @@
 const generateElement = async () => {
-    const apiURL = "https://neelpatel05.pythonanywhere.com/"
+    const apiURL = "https://periodic-table-api.herokuapp.com/"
     const mainData = await getData(apiURL)
     
-    const mainTable = document.querySelector("#main-table")
+    const mainTable = document.querySelector("#main-container")
     mainTable.innerHTML = ""
     mainData.forEach(data => {
         let newElement = `<div onclick="openOverlay(this.id)" id="${data.symbol}">${data.symbol}</div>`
@@ -11,14 +11,16 @@ const generateElement = async () => {
     });
     stopLoadingAnimation();
 }
+
 const getData = (apiURL) => {
     return fetch(apiURL)
             .then(response => response.json())
             .then(response => response)
             .catch(err => err)
 }
+
 const openOverlay = (elementSymbol) => {
-    refreshElementInformation(elementSymbol);
+    updateElementInformation(elementSymbol);
     
     const mainOverlay = document.querySelector("#main-overlay")
     const body = document.querySelector("body")
@@ -26,43 +28,58 @@ const openOverlay = (elementSymbol) => {
     mainOverlay.classList.remove("opacity-0")
     mainOverlay.classList.remove("pointer-events-none")
 }
+
 const closeOverlay = () => {
     const mainOverlay = document.querySelector("#main-overlay")
     const body = document.querySelector("body")
     body.classList.remove("overflow-hidden")
     mainOverlay.classList.add("opacity-0")
     mainOverlay.classList.add("pointer-events-none")
+
+    // reset all data
+    resetElementInformation()
 }
-const refreshElementInformation = async (elementSymbol) => {
-    const elementData = await getData(`https://neelpatel05.pythonanywhere.com/element/symbol?symbol=${elementSymbol}`)
+
+const updateElementInformation = async (elementSymbol) => {
+    const elementData = await getData(`https://periodic-table-api.herokuapp.com/atomicSymbol/${elementSymbol}`)
 
     const mainOverlayContent = document.querySelector("#overlay-content")
-    const refreshedContent = `
+    const updatedContent = `
         <header>
-            <h1 class="capitalize font-bold text-4xl">${isEmpty(elementData.symbol)}</h1>
-            <h2 class="capitalize text-2xl">${isEmpty(elementData.name)}</h2>
+            <h1 class="capitalize font-bold text-4xl element-data">${isEmpty(elementData.symbol)}</h1>
+            <h2 class="capitalize text-2xl element-data">${isEmpty(elementData.name)}</h2>
         </header>
 
         <main>
             <p class="text-lg text-gray-700 capitalize">
-                Atomic Number : <span class="text-black">${isEmpty(elementData.atomicNumber)}</span>
+                Atomic Number : <span class="text-black element-data">${isEmpty(elementData.atomicNumber)}</span>
             </p>
             <p class="text-lg text-gray-700 capitalize">
-                Atomic Mass : <span class="text-black">${isEmpty(elementData.atomicMass)}</span>
+                Atomic Mass : <span class="text-black element-data">${isEmpty(elementData.atomicMass)}</span>
             </p>
             <p class="text-lg text-gray-700 capitalize">
-                Electronic Configuration : <span class="text-black">${isEmpty(elementData.electronicConfiguration)}</span>
+                Electronic Configuration : <span class="text-black element-data">${isEmpty(elementData.electronicConfiguration)}</span>
             </p>
             <p class="text-lg text-gray-700 capitalize">
-                Electron Affinity : <span class="text-black">${isEmpty(elementData.electronAffinity)}</span>
+                Electron Affinity : <span class="text-black element-data">${isEmpty(elementData.electronAffinity)}</span>
             </p>
             <p class="text-lg text-gray-700 capitalize">
-                Ionization Energy : <span class="text-black">${isEmpty(elementData.ionizationEnergy)}</span>
+                Ionization Energy : <span class="text-black element-data">${isEmpty(elementData.ionizationEnergy)}</span>
             </p>
         </main>
     `
-    mainOverlayContent.innerHTML = refreshedContent
+    mainOverlayContent.innerHTML = updatedContent
 }
+
+const resetElementInformation = () => {
+    const elementsData = document.querySelectorAll("#overlay-content .element-data")
+
+    // delete all previous element data 
+    elementsData.forEach(elData => {
+        elData.innerHTML = ""
+    })
+}
+
 const isEmpty = (string) => {
     return string === "" ? "-" : string
 }
